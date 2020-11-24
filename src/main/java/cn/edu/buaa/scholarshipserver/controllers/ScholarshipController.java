d8d0cf62de7aa8a2ad609e22da9ebf88217ea49b
h56983577 <h56983577@126.com>
2020-11-24 20:14:44 +0800

    init_添加swagger UI 相关配置

--- src/main/java/cn/edu/buaa/scholarshipserver/controllers/ScholarshipController.java
+++ src/main/java/cn/edu/buaa/scholarshipserver/controllers/ScholarshipController.java
@@ -2,11 +2,14 @@ package cn.edu.buaa.scholarshipserver.controllers;
 
 import cn.edu.buaa.scholarshipserver.services.ScholarshipService;
 import cn.edu.buaa.scholarshipserver.utils.Response;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Api(value = "ScholarshipController")
 public class ScholarshipController {
 
     ScholarshipService scholarshipService;
@@ -16,6 +19,7 @@ public class ScholarshipController {
     }
 
     @GetMapping("/hello")
+    @ApiOperation(notes = "hello!", value = "一个示例接口，可通过此查看响应体格式")
     public ResponseEntity<Response> hello() {
         return scholarshipService.getHello();
     }
