4ac8757f14aee8302494a973678fbd937fb388f2
JerryAstro2049 <jerryloi@163.com>
2020-12-03 23:37:29 +0800

    add MessageController

---
+++ src/main/java/cn/edu/buaa/scholarshipserver/controllers/MessageController.java
@@ -0,0 +1,63 @@
+package cn.edu.buaa.scholarshipserver.controllers;
+
+import cn.edu.buaa.scholarshipserver.services.message.MessageService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/message")
+public class MessageController {
+    private final MessageService messageService;
+
+    MessageController(MessageService messageService) {
+        this.messageService = messageService;
+    }
+
+    @PostMapping("/send")
+    public int sendMessage(String msgtitle, String msgcontent, Integer sender_userid, Integer receiver_userid) {
+        return messageService.sendMessage(msgtitle, msgcontent, sender_userid, receiver_userid);
+    }
+
+}
+
+/*
+    ***MODEL***
+
+    Message
+    (
+        private Integer msgid;
+        private Long paperid;
+        private Long patentid;
+        private Long projectid;
+        private Integer userid;
+        private Integer useUserid;
+        private String msgtitle;
+        private String msgcontent;
+        private Integer msgstatus;
+        private Date sendtime;
+        private Integer msgtype;
+    );
+
+    ***MODEL END***
+
+    ***Database Table***
+
+    Message
+    (
+        MsgId                int not null,
+        PaperId              bigint,
+        PatentId             bigint,
+        ProjectId            bigint,
+        UserID               int,
+        Use_UserID           int,
+        MsgTitle             varchar(30) not null,
+        MsgContent           varchar(500) not null,
+        MsgStatus            int not null,
+        SendTime             datetime not null,
+        MsgType              int not null,
+        primary key (MsgId)
+    );
+
+    ***Database Table END***
+*/
