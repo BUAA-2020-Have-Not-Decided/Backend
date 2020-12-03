1f367938f51fe67c251745761e010e656838f404
wy672515431 <672515431@qq.com>
2020-12-03 21:37:42 +0800

    1
    1

--- src/main/java/cn/edu/buaa/scholarshipserver/dao/ScholarDAO.java
+++
@@ -1,8 +0,0 @@
-package cn.edu.buaa.scholarshipserver.dao;
-
-import cn.edu.buaa.scholarshipserver.models.Scholar;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-
-public interface ScholarDAO extends ElasticsearchRepository<Scholar,Long> {
-    Scholar findByscholarid(int ScholarId);
-}
--- src/main/java/cn/edu/buaa/scholarshipserver/dao/ScholarDao.java
+++ src/main/java/cn/edu/buaa/scholarshipserver/dao/ScholarDao.java
@@ -3,6 +3,9 @@ package cn.edu.buaa.scholarshipserver.dao;
 import cn.edu.buaa.scholarshipserver.models.Scholar;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
-public interface ScholarDAO extends ElasticsearchRepository<Scholar,Long> {
-    Scholar findByscholarid(int ScholarId);
+import java.util.List;
+
+public interface ScholarDao extends ElasticsearchRepository<Scholar,Long> {
+    public Scholar findByScholarId(int ScholarId);
+    public List<Scholar> findByNormalizedName(String normalizedname);
 }
