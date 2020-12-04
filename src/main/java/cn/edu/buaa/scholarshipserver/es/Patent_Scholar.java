9653ffce9bb7179b5436793c8c4a2e089c29442d
firefly668 <49812001+firefly668@users.noreply.github.com>
2020-12-04 15:59:49 +0800

    update Patent & Patent_Scholar

--- src/main/java/cn/edu/buaa/scholarshipserver/es/Patent_Scholar.java
+++ src/main/java/cn/edu/buaa/scholarshipserver/es/Patent_Scholar.java
@@ -1,4 +1,24 @@
 package cn.edu.buaa.scholarshipserver.es;
 
+import io.swagger.models.auth.In;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+//@Document(indexName = )
 public class Patent_Scholar {
+    @Id
+    @Field(type = FieldType.Auto)
+    private Integer id;
+    @Field(type = FieldType.Auto)
+    private Integer patentId;
+    @Field(type = FieldType.Auto)
+    private Integer scholarId;
 }
