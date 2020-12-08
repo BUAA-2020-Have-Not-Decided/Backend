ae5d69d17ee8cc3821b7d1efd4c4c0f4a4770d7d
Dench991228 <denghanzhi85139@163.com>
2020-12-08 19:40:16 +0800

    夺回了我被别人注释掉的工作劳动果实，以及添加了shiro依赖

--- src/main/java/cn/edu/buaa/scholarshipserver/controllers/UserController.java
+++ src/main/java/cn/edu/buaa/scholarshipserver/controllers/UserController.java
@@ -11,12 +11,12 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 import java.util.TreeMap;
 
-/*@RestController
+@RestController
 @RequestMapping("/user")
 @Api(tags = {"用户相关接口"})
 public class UserController {
-    //@Autowired
-    //private UserService user_service;
+    @Autowired
+    private UserService user_service;
 
     @Autowired
     private EmailSender email_sender;
@@ -48,27 +48,27 @@ public class UserController {
     public Map<String, Object> register(@RequestParam("Name") String username, @RequestParam("Email") String email, @RequestParam("Password") String password){
         Map<String, Object> result = new TreeMap<>();
         if(user_service.emailUsed(email)){
-            result.put("message", "这个邮箱已经被用过了");
+            result.put("Message", "这个邮箱已经被用过了");
         }
         else if(user_service.usernameUsed(username)){
-            result.put("message", "这个用户名已经被用过了");
+            result.put("Message", "这个用户名已经被用过了");
         }
         else{
             try{
                 String code = digest_util.getRandMD5Code(email);
                 email_sender.sendEmail(email, code);
                 user_service.register(username, password, email, code);
-                result.put("message", String.format("验证链接已发送到%s，链接10分钟内有效", email));
-                result.put("status",true);
+                result.put("Message", String.format("验证链接已发送到%s，链接10分钟内有效", email));
+                result.put("Status",true);
                 return result;
             }
             catch(Exception e){
-                result.put("message", "邮件发送失败");
-                result.put("status", false);
+                result.put("Message", "邮件发送失败");
+                result.put("Status", false);
                 return result;
             }
         }
-        result.put("status", false);
+        result.put("Status", false);
         return result;
     }
     //用户验证的地方
@@ -76,25 +76,24 @@ public class UserController {
     public Map<String, Object>verifyUser(@RequestParam("Code") String code){
         Map<String, Object> result = new TreeMap<>();
         int verify_status = user_service.verify(code);
-        result.put("status", verify_status > 0);
+        result.put("Status", verify_status > 0);
         switch(verify_status){
             case -1:
-                result.put("message", "用户名被人抢注了");
+                result.put("Message", "用户名被人抢注了");
                 break;
             case 0:
-                result.put("message", "验证链接有误");
+                result.put("Message", "验证链接有误");
                 break;
             case 1:
-                result.put("message", "邮箱已注册，请直接登录");
+                result.put("Message", "邮箱已注册，请直接登录");
                 break;
             case 2:
-                result.put("message", "验证成功");
+                result.put("Message", "验证成功");
                 break;
             default:
-                result.put("message", "未知错误");
+                result.put("Message", "未知错误");
                 break;
         }
         return result;
     }
 }
-*/
