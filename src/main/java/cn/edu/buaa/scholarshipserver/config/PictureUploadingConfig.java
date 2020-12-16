ccebfe338beab87e70b72e5210b40f88c3bead2d
Jerry Loi <45759373+JerryAstro2049@users.noreply.github.com>
2020-12-16 17:23:45 +0800

    Full Support for Appealing (#18)
    * picture uploading in messaging(90% completed)
    
    * add pictures folder mapping

---
+++ src/main/java/cn/edu/buaa/scholarshipserver/config/PictureUploadingConfig.java
@@ -0,0 +1,18 @@
+package cn.edu.buaa.scholarshipserver.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class PictureUploadingConfig implements WebMvcConfigurer {
+    @Value("${files.path}")
+    private String filePath;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/pictures/**").addResourceLocations("file:" + filePath);
+    }
+
+}
--- src/main/java/cn/edu/buaa/scholarshipserver/services/message/MessageService.java
+++ src/main/java/cn/edu/buaa/scholarshipserver/services/message/MessageService.java
@@ -6,12 +6,11 @@ import cn.edu.buaa.scholarshipserver.utils.Response;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.util.FileCopyUtils;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Base64;
 import java.util.Date;
 import java.util.List;
 import java.util.UUID;
@@ -85,7 +84,7 @@ public class MessageService {
     public ResponseEntity<Response> makeAppeal(Integer userId,
                                                Long scholarshipId,
                                                String scholarshipType,
-                                               MultipartFile complaintMaterial,
+                                               String complaintMaterial,
                                                String messageTitle,
                                                String messageContent) {
         try {
@@ -103,8 +102,15 @@ public class MessageService {
                 default:
                     System.out.println("wrong parameter name");
             }
-            String complaintMaterialFilePath = uploadFile(complaintMaterial);
-            newMessage.setComplaintMaterialUrl(complaintMaterialFilePath);
+            if (complaintMaterial != null) {
+                try {
+                    String complaintMaterialFilePath = uploadImage(complaintMaterial);
+                    newMessage.setComplaintMaterialUrl(complaintMaterialFilePath);
+                }
+                catch (Exception e) {
+                    return ResponseEntity.ok(new Response(500, e.getMessage(), ""));
+                }
+            }
             messageMapper.insertSelective(newMessage);
             return ResponseEntity.ok(new Response("done", ""));
         } catch (Exception e) {
@@ -112,28 +118,56 @@ public class MessageService {
         }
     }
 
-    private String uploadFile(MultipartFile file) {
-        String newFileName = "";
-        try {
-            String originalFilename = file.getOriginalFilename();
-            if (originalFilename != null) {
-                String extensionName = originalFilename.substring(originalFilename.lastIndexOf("."));
-                newFileName = UUID.randomUUID().toString() + extensionName;
+    public String uploadImage(String base64Data) throws Exception {
+        String dataPrix = ""; //base64格式前头
+        String data = ""; //实体部分数据
+        if (base64Data == null || base64Data.equals("")) {
+            throw new Exception("empty picture");
+        }
+        else {
+            String[] d = base64Data.split("base64,"); //将字符串分成数组
+            if (d.length == 2) {
+                dataPrix = d[0];
+                data = d[1].replace(" ", "+");
             }
             else {
-                System.out.println("The file doesn't have an original file name?!");
-                newFileName = UUID.randomUUID().toString();
+                throw new Exception("invalid picture");
             }
-            try {
-                FileCopyUtils.copy(file.getInputStream(), new FileOutputStream(new File(filePath + newFileName)));
-            } catch (IOException e) {
-                System.out.println("Something went wrong when writing the file to server");
-            }
-        } catch (NullPointerException e) {
-            System.out.println("Didn't get the file");
         }
-
-        return newFileName;
+        String suffix = ""; //图片后缀，用以识别哪种格式数据
+        //data:image/jpeg;base64,base64编码的jpeg图片数据
+        if ("data:image/jpeg;".equalsIgnoreCase(dataPrix)) {
+            suffix = ".jpg";
+        }
+        else if ("data:image/png;".equalsIgnoreCase(dataPrix)) {
+            //data:image/png;base64,base64编码的png图片数据
+            suffix = ".png";
+        }
+        else {
+            throw new Exception("invalid picture type");
+        }
+        String uuid = UUID.randomUUID().toString().replaceAll("-", "");
+        String newFileName = uuid + suffix;
+        String imgFilePath = filePath + newFileName; //新生成的图片
+        Base64.Decoder decoder = Base64.getDecoder();
+        try {
+            //Base64解码
+            byte[] b = decoder.decode(data);
+            for (int i = 0; i < b.length; ++i) {
+                if (b[i] < 0) {
+                    //调整异常数据
+                    b[i] += 256;
+                }
+            }
+            OutputStream out = new FileOutputStream(imgFilePath);
+            out.write(b);
+            out.flush();
+            out.close();
+            return "http://localhost:8086/pictures/" + newFileName;
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new Exception("IOException occurred");
+        }
     }
 
     public ResponseEntity<Response> getAppeals() {
