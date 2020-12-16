b1429f6d7e7cfe8c0df115a44a2e2782c15b0c46
h56983577 <h56983577@126.com>
2020-12-16 09:30:20 +0800

    init_添加了JUnit单元测试相关代码

---
+++ src/test/java/cn/edu/buaa/scholarshipserver/Application.java
@@ -0,0 +1,13 @@
+package cn.edu.buaa.scholarshipserver;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@MapperScan("cn.edu.buaa.scholarshipserver.dao")
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
