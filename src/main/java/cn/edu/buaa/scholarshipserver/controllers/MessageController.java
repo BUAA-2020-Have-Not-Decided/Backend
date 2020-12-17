c0b4e46c9cf9637fbef9fba319a576858362a302
JerryAstro2049 <jerryloi@163.com>
2020-12-17 20:08:43 +0800

    implement interceptor & few apis' routes change

--- src/main/java/cn/edu/buaa/scholarshipserver/controllers/MessageController.java
+++ src/main/java/cn/edu/buaa/scholarshipserver/controllers/MessageController.java
@@ -1,7 +1,9 @@
 package cn.edu.buaa.scholarshipserver.controllers;
 
+import cn.edu.buaa.scholarshipserver.models.User;
 import cn.edu.buaa.scholarshipserver.services.message.MessageService;
 import cn.edu.buaa.scholarshipserver.utils.Response;
+import org.apache.shiro.SecurityUtils;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,47 +19,48 @@ public class MessageController {
     @PostMapping("/user/one")
     public ResponseEntity<Response> sendMessage(String messageTitle,
                                                 String messageContent,
-                                                Integer sender_userid,
                                                 Integer receiver_userid) {
-        return messageService.sendUserMessage(messageTitle, messageContent, sender_userid, receiver_userid);
+        User user = (User) SecurityUtils.getSubject().getPrincipal();
+        return messageService.sendUserMessage(messageTitle, messageContent, user.getUserID(), receiver_userid);
     }
 
     @GetMapping("/user/inbox")
-    public ResponseEntity<Response> getMessages(Integer userId) {
-        return messageService.getMessages(userId);
+    public ResponseEntity<Response> getMessages() {
+        User user = (User) SecurityUtils.getSubject().getPrincipal();
+        return messageService.getMessages(user.getUserID());
     }
 
-    @GetMapping("/one")
+    @GetMapping("/user/one")
     public ResponseEntity<Response> getMessage(Integer messageId) {
         return messageService.getMessage(messageId);
     }
 
-    @PutMapping("/one")
+    @PutMapping("/user/one")
     public ResponseEntity<Response> markMessageAsRead(Integer messageId) {
         return messageService.markMessageAsRead(messageId);
     }
 
-    @DeleteMapping("/one")
+    @DeleteMapping("/user/one")
     public ResponseEntity<Response> deleteMessage(Integer messageId) {
         return messageService.deleteMessage(messageId);
     }
 
-    @PostMapping("/appeal/one")
-    public ResponseEntity<Response> makeAppeal(Integer userId,
-                                               Long towardsId,
+    @PostMapping("/scholar/appeal/one")
+    public ResponseEntity<Response> makeAppeal(Long towardsId,
                                                String towardsType,
                                                String complaintMaterial,
                                                String messageTitle,
                                                String messageContent) {
-        return messageService.makeAppeal(userId, towardsId, towardsType, complaintMaterial, messageTitle, messageContent);
+        User user = (User) SecurityUtils.getSubject().getPrincipal();
+        return messageService.makeAppeal(user.getUserID(), towardsId, towardsType, complaintMaterial, messageTitle, messageContent);
     }
 
-    @GetMapping("/appeal/all")
+    @GetMapping("/admin/appeal/all")
     public ResponseEntity<Response> getAppeals() {
         return messageService.getAppeals();
     }
 
-    @PutMapping("/appeal/one")
+    @PutMapping("/admin/appeal/one")
     public ResponseEntity<Response> updateAppealMessageStatus(Integer messageId, Integer messageStatus) {
         return messageService.updateAppealMessageStatus(messageId, messageStatus);
     }
